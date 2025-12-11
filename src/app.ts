3e7e4d8f5d440489ddc541581561b9ed6ebbc089
Mostafiz <mostafiz4372@gmail.com>
2025-12-11 15:57:43 +0600

    add frontend url in cors

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(express.json()); // Parse incoming JSON requests
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://eventhive-frontend-8eb6.vercel.app"],
     credentials: true,
   })
 );
